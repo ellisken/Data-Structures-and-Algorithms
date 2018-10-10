6f2889a2b6f08731e63bdc190e2965a54baa020e
ellisken <kse104@gmail.com>
2018-10-09 17:15:24 -0700

    add code to ArrayShift

--- Challenges/arrayShift/arrayShift/Program.cs
+++ Challenges/arrayShift/arrayShift/Program.cs
@@ -6,7 +6,31 @@ namespace arrayShift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declare some test arrays
+            int[] arr1 = { 3, 1, -2, 94 }, arr2 = { 4, 5, 6 }, arr3 = { 1 }, arr4 = { };
+     
+            
+        }
+
+
+        static int[] ArrayShift(int[] arr, int num)
+        {
+            //Create new array
+            int[] newArr = new int[arr.Length + 1];
+
+            //Find middle index
+            int middleIndex = arr.Length / 2;
+            if (arr.Length % 2 != 0) middleIndex += 1;
+
+            //Fill new array
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (i < middleIndex) newArr[i] = arr[i];
+                else if (i == middleIndex) newArr[i] = num;
+                else newArr[i] = arr[i - 1];
+            }
+
+            return newArr;
         }
     }
 }
