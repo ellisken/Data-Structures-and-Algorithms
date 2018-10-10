1b91bf6fdc4277c39745af0c6296d768dcb2830d
ellisken <kse104@gmail.com>
2018-10-10 15:42:40 -0700

    add stuff to main

--- Challenges/BinarySearch/BinarySearch/Program.cs
+++ Challenges/BinarySearch/BinarySearch/Program.cs
@@ -7,7 +7,13 @@ namespace BinarySearch
         static void Main()
         {
             int[] testArray = { 0, 5, 10, 23, 41 };
-            Console.WriteLine("{0}", BinarySearch(testArray, 5));
+            Console.WriteLine($"Array: [{string.Join(",", testArray)}]");
+            Console.WriteLine("Index of 0: {0}", BinarySearch(testArray, 0));
+            Console.WriteLine("Index of 5: {0}", BinarySearch(testArray, 5));
+            Console.WriteLine("Index of 10: {0}", BinarySearch(testArray, 10));
+            Console.WriteLine("Index of 23: {0}", BinarySearch(testArray, 23));
+            Console.WriteLine("Index of 41: {0}", BinarySearch(testArray, 41));
+            Console.WriteLine("Index of 99: {0}", BinarySearch(testArray, 99));
         }
 
         /// <summary>
