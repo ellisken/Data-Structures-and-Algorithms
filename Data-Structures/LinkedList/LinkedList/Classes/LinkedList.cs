354d775b96498e082d77c02597b82bf8749b29e0
ellisken <kse104@gmail.com>
2018-10-15 13:43:35 -0700

    define node and ll classes

---
+++ Data-Structures/LinkedList/LinkedList/Classes/LinkedList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Classes
+{
+    class LinkedList
+    {   
+        /// <summary>
+        /// First node in the linked list
+        /// </summary>
+        public Node Head { get; set; }
+
+        /// <summary>
+        /// Always points to current node in the list
+        /// </summary>
+        public Node Current { get; set; }
+
+        public LinkedList(Node node)
+        {
+            Head = node;
+            Current = node;
+        }
+
+        /// <summary>
+        /// Adds a node at the beginning of the LL, O(1)
+        /// </summary>
+        /// <param name="node"></param>
+        public void Insert(Node node)
+        {
+            node.Next = Head;
+            Head = node;
+            Current = Head;
+        }
+
+        public bool Includes(int value)
+        {
+            Current = Head;
+            while(Current != null)
+            {
+                if (Current.Value == value) return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            Current = Head;
+            while(Current != null)
+            {
+                Console.WriteLine($"{Current.Value}-->");
+                Current = Current.Next;
+            }
+        }
+    }
+}
---
+++ Data-Structures/LinkedList/LinkedList/Classes/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList.Classes
+{
+    class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(int value, Node next)
+        {
+            Value = value;
+        }
+    }
+}
