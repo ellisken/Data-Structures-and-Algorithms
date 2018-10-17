de88b2df1493a5d1a8b0878c146a30994014cadc
ellisken <kse104@gmail.com>
2018-10-17 16:11:07 -0700

    initial setup

---
+++ Challenges/LLMerge/LLMerge/Program.cs
@@ -0,0 +1,21 @@
+﻿using LinkedList.Classes;
+using System;
+
+namespace LLMerge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static LList Merge(LList l1, LList l2)
+        {
+            LList newList = null;
+
+
+            return newList;
+        }
+    }
+}
---
+++ Challenges/LLMerge/TestMerge/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using LLMerge;
+using Xunit;
+
+namespace TestMerge
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
