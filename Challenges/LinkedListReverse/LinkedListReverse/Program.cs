6d9e0bcee383bb40aa849b3af10adde1a37136ed
ellisken <kse104@gmail.com>
2018-10-19 11:14:03 -0700

    reverse implemented

---
+++ Challenges/LinkedListReverse/LinkedListReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LinkedList.Classes;
+
+namespace LinkedListReverse
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LList myList = new LList(new Node(1));
+        }
+
+
+        public static void LinkedListReverse(LList list)
+        {
+            //Create pointers to track position
+            Node current = list.Head, prev = null, next = null;
+
+            list.Current = list.Head;
+            while(list.Current != null)
+            {
+                next = list.Current.Next;
+                list.Current.Next = prev;
+                prev = list.Current;
+                list.Current = list.Current.Next;
+            }
+            list.Head = prev;
+        }
+    }
+}
---
+++ Challenges/LinkedListReverse/TestLLReverse/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using LinkedList.Classes;
+using Xunit;
+
+namespace TestLLReverse
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
