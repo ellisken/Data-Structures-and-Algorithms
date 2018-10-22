c14f4495de34c4380b35cd362bbe587d8ce4fe9f
ellisken <kse104@gmail.com>
2018-10-22 15:35:10 -0700

    node and stack implemente

--- Data-Structures/StackAndQueue/StackAndQueue/Classes/Node.cs
+++ Data-Structures/StackAndQueue/StackAndQueue/Classes/Node.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace StackAndQueue.Classes
 {
-    class Node
+    public class Node
     {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+
+        //Constructor requires value for a node
+        public Node(object value)
+        {
+            Value = value;
+        }
     }
 }
