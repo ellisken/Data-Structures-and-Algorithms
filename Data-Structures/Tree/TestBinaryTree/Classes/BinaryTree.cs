7270bdbe2ed3bdf3f8c1d1fe7a74cb2d33129b50
ellisken <kse104@gmail.com>
2018-10-27 22:03:58 -0700

    setup Node and started BinaryTree classes

---
+++ Data-Structures/Tree/TestBinaryTree/Classes/BinaryTree.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestBinaryTree.Classes
+{
+    class BinaryTree
+    {
+        Node root;
+
+        /// <summary>
+        /// Default contructor that creates an empty tree
+        /// </summary>
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        /// <summary>
+        /// Creates a new tree with a given root value
+        /// </summary>
+        /// <param name="value"></param>
+        public BinaryTree(int value)
+        {
+            root = new Node(value);
+        }
+    }
+}
---
+++ Data-Structures/Tree/TestBinaryTree/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestBinaryTree
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
