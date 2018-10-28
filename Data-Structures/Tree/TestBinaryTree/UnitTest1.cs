d654ca7114cd60b6b904b2c8fdb449fc79259060
ellisken <kse104@gmail.com>
2018-10-28 16:31:04 -0700

    pre, post, and inorder tests done

--- Data-Structures/Tree/TestBinaryTree/UnitTest1.cs
+++ Data-Structures/Tree/TestBinaryTree/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 using Tree.Classes;
-
+using System.Collections.Generic;
 
 namespace TestBinaryTree
 {
@@ -76,8 +76,53 @@ namespace TestBinaryTree
             }
             Assert.Null(bst.Search(bst.Root, 5));
         }
+
         //Test BT Preorder 1 node, 3 different values
+        [Theory]
+        [InlineData (1, 2, 3)]
+        [InlineData (4, 5, 6)]
+        [InlineData (12, 6, 3)]
+        public void TestPreorder(int v1, int v2, int v3)
+        {
+            List<int> values = new List<int>();
+            List<int> expected = new List<int> { v1, v2, v3 };
+            BinaryTree t = new BinaryTree(v1);
+            t.Root.Left = new Node(v2);
+            t.Root.Right = new Node(v3);
+            t.PreOrder(t.Root, values);
+            Assert.Equal(expected, values);
+        }
+
         //Test BT Inorder 1 node, 3 different values
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 5, 6)]
+        [InlineData(12, 6, 3)]
+        public void TestInorder(int v1, int v2, int v3)
+        {
+            List<int> values = new List<int>();
+            List<int> expected = new List<int> { v2, v1, v3 };
+            BinaryTree t = new BinaryTree(v1);
+            t.Root.Left = new Node(v2);
+            t.Root.Right = new Node(v3);
+            t.InOrder(t.Root, values);
+            Assert.Equal(expected, values);
+        }
+
         //Test BT Postorder 1 node, 3 different values
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 5, 6)]
+        [InlineData(12, 6, 3)]
+        public void TestPostorder(int v1, int v2, int v3)
+        {
+            List<int> values = new List<int>();
+            List<int> expected = new List<int> { v2, v3, v1 };
+            BinaryTree t = new BinaryTree(v1);
+            t.Root.Left = new Node(v2);
+            t.Root.Right = new Node(v3);
+            t.PostOrder(t.Root, values);
+            Assert.Equal(expected, values);
+        }
     }
 }
