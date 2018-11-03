fda113de546f6ffe5343f6324339b29a22e6ba88
ellisken <kse104@gmail.com>
2018-11-03 14:11:29 -0700

    implementation done

---
+++ Challenges/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Tree.Classes;
+
+namespace FindMaxBinaryTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maximum value of a binary tree.");
+        }
+
+        public static int FindMaxValue(BinaryTree tree)
+        {
+            Queue<Node> q = new Queue<Node>();
+            int max = int.MinValue;
+
+            if (tree.Root == null) return max;
+            else
+            {
+                q.Enqueue(tree.Root);
+                while (q.Count > 0)
+                {
+                    Node current = q.Dequeue();
+                    if (current.Value > max) max = current.Value;
+                    if (current.Left != null) q.Enqueue(current.Left);
+                    if (current.Right != null) q.Enqueue(current.Right);
+                }
+            }
+            return max;
+        }
+    }
+}
