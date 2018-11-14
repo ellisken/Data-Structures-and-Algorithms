9d839567fcf501f9f135db72ffd018450d865a9d
ellisken <kse104@gmail.com>
2018-11-14 14:24:43 -0800

    added contents to Main()

--- Challenges/RepeatedWord/RepeatedWord/Program.cs
+++ Challenges/RepeatedWord/RepeatedWord/Program.cs
@@ -7,28 +7,38 @@ namespace RepeatedWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Find a repeated word:");
+            string book = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...";
+            Console.WriteLine($"Test book is: {book}");
+            Console.WriteLine();
+            Console.WriteLine("First duplicate word:");
+            Console.WriteLine($"{RepeatedWord(book)}");
         }
 
         public static string RepeatedWord(string book)
         {
             HashTable words = new HashTable(100);
             char[] charsToTrim = { ',', '.', '?', '!' };
+            //Get individual words
             string[] tokens = book.Split(" ");
+            //Trim special chars and normalize
             for(int i=0; i < tokens.Length; i++)
             {
                 tokens[i] = tokens[i].Trim(charsToTrim).ToLower();
             }
 
+            //Process tokens
             for(int j=0; j < tokens.Length; j++)
             {
+                //Check if word is already in dictionary, return word
                 if (words.Contains(tokens[j]))
                 {
                     return tokens[j];
                 }
+                //Else add token to words dictionary
                 else
                 {
-                    words.Add(tokens[j], true);
+                    if(tokens[j] != "") words.Add(tokens[j], true);
                 }
             }
             return "No Duplicates";
