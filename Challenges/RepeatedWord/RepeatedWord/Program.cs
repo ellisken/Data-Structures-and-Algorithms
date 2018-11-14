735bae3a5245bc9410814e16b40ab86169603fc1
ellisken <kse104@gmail.com>
2018-11-14 14:20:10 -0800

    implementation of RepeatedWord() done

---
+++ Challenges/RepeatedWord/RepeatedWord/Program.cs
@@ -0,0 +1,37 @@
+﻿using Hashtables.Classes;
+using System;
+
+namespace RepeatedWord
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string RepeatedWord(string book)
+        {
+            HashTable words = new HashTable(100);
+            char[] charsToTrim = { ',', '.', '?', '!' };
+            string[] tokens = book.Split(" ");
+            for(int i=0; i < tokens.Length; i++)
+            {
+                tokens[i] = tokens[i].Trim(charsToTrim).ToLower();
+            }
+
+            for(int j=0; j < tokens.Length; j++)
+            {
+                if (words.Contains(tokens[j]))
+                {
+                    return tokens[j];
+                }
+                else
+                {
+                    words.Add(tokens[j], true);
+                }
+            }
+            return "No Duplicates";
+        }
+    }
+}
