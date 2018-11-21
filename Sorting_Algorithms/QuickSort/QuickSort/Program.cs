61eb576c4a2a471e93463ee3f501fce8c58e1af7
ellisken <kse104@gmail.com>
2018-11-21 12:25:57 -0800

    start quicksort

---
+++ Sorting_Algorithms/QuickSort/QuickSort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quicksort:");
+        }
+
+        static void QuickSort(int[] arr, int left, int right)
+        {
+            if(left < right)
+            {  
+                //Partition and get position of pivot
+                int position = Partition(arr, left, right);
+                //Sort of bottom half recursively
+                QuickSort(arr, left, position - 1);
+                //Sort the top half recursively
+                QuickSort(arr, position + 1, right);
+            }
+        }
+
+
+    }
+}
