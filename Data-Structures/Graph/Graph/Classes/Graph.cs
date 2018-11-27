883bde7fd1fff916a4871f3db4d7dfa941cd8b48
ellisken <kse104@gmail.com>
2018-11-26 17:40:52 -0800

    define Graph class

---
+++ Data-Structures/Graph/Graph/Classes/Graph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Graph.Classes.Vertex;
+
+namespace Graph.Classes
+{
+    public class Graph
+    {
+        public int VertexCount { get; set; }
+        public List<Vertex> Vertices { get; set; }
+    }
+}
--- Data-Structures/Graph/Graph/Classes/Vertex.cs
+++ Data-Structures/Graph/Graph/Classes/Vertex.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Graph
+namespace Graph.Classes
 {
     public class Vertex
     {
