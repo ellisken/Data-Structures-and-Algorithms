b2480798e32fac77b99e8ca4e56fec5d1e8be4ce
ellisken <kse104@gmail.com>
2018-11-26 18:06:09 -0800

    implemented AddEdge()

--- Data-Structures/Graph/Graph/Classes/Graph.cs
+++ Data-Structures/Graph/Graph/Classes/Graph.cs
@@ -9,6 +9,10 @@ namespace Graph.Classes
         public int VertexCount { get; set; }
         public List<Vertex> Vertices { get; set; }
         
+        /// <summary>
+        /// Creates a graph with unconnected nodes
+        /// </summary>
+        /// <param name="values">A list of values to give the graph nodes</param>
         public Graph(List<object> values)
         {
             //For each item in the list of values, create a Vertex and add it to the Vertices list
@@ -17,5 +21,30 @@ namespace Graph.Classes
                 Vertices.Add(new Vertex(value));
             }
         }
+
+        public void AddEdge(object v1, object v2, int weight)
+        {
+            //Find the v1 in the vertex list by value
+            Vertex v1Ref = Vertices.Find(x => x.Value == v1);
+            if(v1Ref == null)
+            {
+                v1Ref = new Vertex(v1);
+                Vertices.Add(v1Ref);
+            }
+            Vertex v2Ref = Vertices.Find(x => x.Value == v2);
+            if(v2Ref == null)
+            {
+                v2Ref = new Vertex(v2);
+                Vertices.Add(v2Ref);
+            }
+
+            //Add key:value pair from v1 to v2 with given weight, and vice versa
+            v1Ref.AdjacentVertices.Add(v2Ref, weight);
+            v2Ref.AdjacentVertices.Add(v1Ref, weight);
+        }
+
+
+
+
     }
 } 
