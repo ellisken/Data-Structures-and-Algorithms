83b357ce87cffa47084845753fe50bc6d8ad5f87
ellisken <kse104@gmail.com>
2018-11-28 13:33:53 -0800

    Main written

--- Challenges/GetEdges/GetEdges/Program.cs
+++ Challenges/GetEdges/GetEdges/Program.cs
@@ -9,6 +9,31 @@ namespace GetEdges
         static void Main(string[] args)
         {
             Console.WriteLine("Trip Itinerary");
+            Console.WriteLine("I want to go from Seattle to Denver to LA...");
+            Console.WriteLine("Seattle to Denver costs $300 and Denver to LA costs $200");
+            //Create itinerary array
+            string[] cities = { "Seattle", "Denver", "LA"};
+            List<object> nodes = new List<object>()
+            {
+                "Seattle",
+                "Denver",
+                "LA"
+            };
+
+            //Create graph
+            UndirectedGraph g = new UndirectedGraph(nodes);
+
+            //Add edges
+            g.AddEdge("Seattle", "Denver", 300);
+            g.AddEdge("Denver", "LA", 200);
+
+            //Run GetEdge
+            Console.WriteLine("Running GetEdge...");
+            Tuple<bool, int> result = GetEdge(g, cities);
+
+            //Check that it worked
+            Console.WriteLine("Expected: true, $500");
+            Console.WriteLine($"Result: {result.Item1}, ${result.Item2}");
         }
 
         static Tuple<bool, int> GetEdge(UndirectedGraph g, string[] cities)
